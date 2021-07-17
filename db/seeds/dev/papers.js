37eadc6056fe5169c5c4153e02da7b1af98c1926
Cameron Romo <cromo1225@gmail.com>
2021-07-17 17:42:58 -0600

    Dynamic seeding of large data set

--- db/seeds/dev/papers.js
+++ db/seeds/dev/papers.js
@@ -1,15 +1,35 @@
+const papersData = require('../../../papersData');
+
+const createPaper = async (knex, paper) => {
+  const paperId = await knex('papers').insert({
+    title: paper.title,
+    author: paper.author
+  }, 'id');
+
+  let footnotePromises = paper.footnotes.map(footnote => {
+    return createFootnote(knex, {
+      note: footnote,
+      paper_id: paperId[0]
+    })
+  });
+
+  return Promise.all(footnotePromises);
+}
+
+const createFootnote = (knex, footnote) => {
+  return knex('footnotes').insert(footnote);
+}
+
 exports.seed = async function(knex) {
   try {
     await knex('footnotes').del()
     await knex('papers').del()
 
-    const paperId = await knex('papers').insert({
-      title: 'Fooo', author: 'Bob', publisher: 'Minnesota'
-    }, 'id')
-    return knex('footnotes').insert([
-      { note: 'Lorem', paper_id: paperId[0] },
-      { note: 'Dolor', paper_id: paperId[0] },
-    ])
+    let paperPromises = papersData.map(paper => {
+      return createPaper(knex, paper);
+    });
+
+    return Promise.all(paperPromises);
   } catch(error) {
     console.error(`Error seeding data: ${error}`);
   }
